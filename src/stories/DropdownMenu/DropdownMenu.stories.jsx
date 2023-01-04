bcc6f4e9f44eba087418cb4e8ed4dc2e1aabd003
Feng Xia <feng.xia@lucidum.io>
2023-01-04 09:45:28 -0500

    feat: Add DropdownMenu

---
+++ src/stories/DropdownMenu/DropdownMenu.stories.jsx
@@ -0,0 +1,68 @@
+import { faker } from "@faker-js/faker";
+import React from "react";
+
+import DraftsIcon from "@mui/icons-material/Drafts";
+import InboxIcon from "@mui/icons-material/Inbox";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import DropdownMenu from "./index";
+
+export default {
+  title: "DropdownMenu",
+  component: DropdownMenu,
+};
+
+const BasicList = () => (
+  <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <nav aria-label="main mailbox folders">
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </nav>
+    <Divider />
+    <nav aria-label="secondary mailbox folders">
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary="Trash" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </nav>
+  </Box>
+);
+
+const Template = (args) => <DropdownMenu {...args} />;
+
+export const Example = Template.bind({});
+Example.args = {
+  title: faker.animal.cat(),
+  content: <BasicList />,
+  keep_open: true,
+};
---
+++ src/stories/DropdownMenu/index.jsx
@@ -0,0 +1,53 @@
+import { isNull, isUndefined } from "lodash";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Box, IconButton, Menu, Typography } from "@mui/material";
+
+const DropdownMenu = (props) => {
+  const { title, content, keep_open } = props;
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  if (isNull(content)) return null;
+
+  return (
+    <Box display="inline">
+      <IconButton
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        aria-label="options"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+        <Typography>{isUndefined(title) ? null : title}</Typography>
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <Box p={3} onClick={isUndefined(keep_open) ? handleClose : null}>
+          {content}
+        </Box>
+      </Menu>
+    </Box>
+  );
+};
+
+DropdownMenu.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.node.isRequired,
+  keep_open: PropTypes.bool,
+};
+
+export default DropdownMenu;
