7d295c2e3a8e8823993aadba4dd430446b7ebed0
Feng Xia <feng.xia@lucidum.io>
2023-01-04 16:33:40 -0500

    feat: Add NotFoundView

--- src/stories/Page/Page.stories.jsx
+++ src/stories/Page/Page.stories.jsx
@@ -1,8 +1,6 @@
 import { faker } from "@faker-js/faker";
 import React from "react";
 
-import Button from "@mui/material/Button";
-
 import Page from "./index";
 
 export default {
@@ -15,5 +13,5 @@ const Template = (args) => <Page {...args} />;
 export const Example = Template.bind({});
 Page.args = {
   title: faker.animal.cat(),
-  children: <Button variant="contained">Display a button</Button>,
+  children: faker.lorem.words(100),
 };
