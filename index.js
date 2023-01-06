5aa4942f9b8c43f4515354332f97e16b6e71caf6
Feng Xia <feng.xia@lucidum.io>
2023-01-06 11:24:10 -0500

    feat: Add components to export

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import ABDonutChart from "./src/stories/ABDonutChart";
 import AsDialog from "./src/stories/AsDialog";
 import AsVerticalStepper from "./src/stories/AsVerticalStepper";
 import ColoredNumber from "./src/stories/ColoredNumber";
+// components
 import CountCards from "./src/stories/CountCards";
 import DictCard from "./src/stories/DictCard";
 import DictTable from "./src/stories/DictTable";
@@ -18,6 +19,33 @@ import Page from "./src/stories/Page";
 import PollResource from "./src/stories/PollResource";
 import RankChart from "./src/stories/RankChart";
 import SendEmail from "./src/stories/SendEmail";
-import ShowResource from "./src/stories/ShowResource/";
+import ShowResource from "./src/stories/ShowResource";
 import SimpleSnackbar from "./src/stories/SimpleSnackbar";
 import TimeSeriesColumnChart from "./src/stories/TimeSeriesColumnChart";
+
+// exports
+export {
+  ABDonutChart,
+  AsDialog,
+  AsVerticalStepper,
+  ColoredNumber,
+  CountCards,
+  DictCard,
+  DictTable,
+  DropdownMenu,
+  GlobalStyles,
+  HighchartGraph,
+  HighlightedText,
+  LightTheme,
+  Logo,
+  MenuBar,
+  MultilineChart,
+  NotFoundView,
+  Page,
+  PollResource,
+  RankChart,
+  SendEmail,
+  ShowResource,
+  SimpleSnackbar,
+  TimeSeriesColumnChart,
+};
