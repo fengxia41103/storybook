c958ba2252c5a7aaf5d1d616cad648170b05b92d
Feng Xia <feng_xia41103@hotmail.com>
2023-03-10 15:13:19 -0500

    refactor: Simplified XKCD

--- src/stories/XKCD/index.jsx
+++ src/stories/XKCD/index.jsx
@@ -2,39 +2,35 @@ import fetchJsonp from "fetch-jsonp";
 import Image from "mui-image";
 import React, { useEffect, useState } from "react";
 
-import { Box, Typography } from "@mui/material";
-
 // 1743 is from manual testing
 const MIN = 1;
 const MAX = 1743;
+const getNextIndex = () => Math.floor(Math.random() * (MAX - MIN) + MIN);
+const getNextImage = () => {
+  const id = getNextIndex();
+  return `https://dynamic.xkcd.com/api-0/jsonp/comic/${id}`;
+};
 
 const XKCD = () => {
-  const [quote, setQuote] = useState(null);
   const [img, setImg] = useState(null);
 
   const getImage = () => {
-    // AJAX
-    const id = Math.floor(Math.random() * (MAX - MIN) + MIN);
-
     // NOTE: must use `https` if using github page, which is
     // served in https (is a setting option, however).
-    const apiUrl = `https://dynamic.xkcd.com/api-0/jsonp/comic/${id}`;
+    const apiUrl = getNextImage();
+
+    // MUST: use jsonp because of localhost will be blocked
+    // by cross-origin rule!
     fetchJsonp(apiUrl)
       .then((resp) => resp.json())
       .then((data) => {
-        setQuote(data.title);
         setImg(data.img);
       });
   };
 
   useEffect(() => getImage(), []);
 
-  return (
-    <Box>
-      <Image src={img} width="60vw" onClick={getImage} />
-      <Typography variant="body1">{quote}</Typography>
-    </Box>
-  );
+  return <Image src={img} width="80vw" onClick={getImage} />;
 };
 
 export default XKCD;
