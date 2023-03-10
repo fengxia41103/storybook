089a116756d8e37f25e5b9ba01a7708622ce0384
Feng Xia <feng_xia41103@hotmail.com>
2023-03-10 14:55:09 -0500

    feat: Add XKCD and new 404 page

---
+++ src/stories/XKCD/XKCD.stories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import XKCD from "./index";
+
+export default {
+  title: "XKCD",
+  component: XKCD,
+};
+
+const Template = () => <XKCD />;
+
+export const Example = Template.bind({});
+Example.args = {};
