04cd39f8245b042cdf0c172f01454ecfb22e823d
Feng Xia <feng_xia41103@hotmail.com>
2023-03-10 19:41:56 -0500

    feat: Add xkcd and external img as background
    - Add xkcd as prop to 404 page
    - Add prop to use external image as background image

--- src/stories/NotFoundView/NotFoundView.stories.jsx
+++ src/stories/NotFoundView/NotFoundView.stories.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-import { ThemeProvider } from "@mui/material";
-
-import LightTheme from "../LightTheme";
-
 import NotFoundView from "./index";
 
 export default {
@@ -11,11 +7,24 @@ export default {
   component: NotFoundView,
 };
 
-const Template = () => (
-  <ThemeProvider theme={LightTheme}>
-    <NotFoundView />
-  </ThemeProvider>
-);
+const Template = (args) => <NotFoundView {...args} />;
 
-export const Example = Template.bind({});
-Example.args = {};
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WhiteFont = Template.bind({});
+WhiteFont.args = {
+  fontColor: "white",
+};
+
+export const ExternalImage = Template.bind({});
+ExternalImage.args = {
+  fontColor: "primary",
+  backgroundImage:
+    "https://static01.nyt.com/images/2018/11/23/arts/23shoplifterspix1/merlin_146950422_cdf94069-a72a-4dfb-ae9d-6d4222e39c29-superJumbo.jpg?quality=75&auto=webp",
+};
+
+export const AddXKCD = Template.bind({});
+AddXKCD.args = {
+  xkcd: true,
+};
--- src/stories/NotFoundView/index.jsx
+++ src/stories/NotFoundView/index.jsx
@@ -1,38 +1,59 @@
+import PropTypes from "prop-types";
 import React from "react";
 
 import { Box, Container, Typography } from "@mui/material";
 
 import Page from "../Page";
+import XKCD from "../XKCD";
 
 import PageNotFoundImage from "./assets/404.jpg";
 
-const NotFoundView = () => (
-  <Page
-    title="404"
-    style={{
-      backgroundImage: `url(${PageNotFoundImage})`,
-      backgroundSize: "cover",
-      height: "100vh",
-      color: "#f5f5f5",
-    }}
-  >
-    <Box
-      display="flex"
-      flexDirection="column"
-      height="100%"
-      justifyContent="center"
-      sx={{ maxWidth: "sm" }}
+const NotFoundView = (props) => {
+  const {
+    fontColor = "primary",
+    backgroundImage = PageNotFoundImage,
+    xkcd = false,
+  } = props;
+
+  return (
+    <Page
+      title="404"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        height: "100vh",
+        color: "#f5f5f5",
+      }}
     >
-      <Container maxWidth="sm">
-        <Typography color="white" variant="h3">
-          Ooops, I do not have what you are looking for.
-        </Typography>
-        <Typography color="white" variant="body1">
-          Come back some other time. I am working it right now.
-        </Typography>
-      </Container>
-    </Box>
-  </Page>
-);
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        justifyContent="center"
+        sx={{ maxWidth: "sm" }}
+      >
+        <Container maxWidth="sm">
+          <Typography color={fontColor} variant="h3">
+            Ooops, I do not have what you are looking for.
+          </Typography>
+          {xkcd ? (
+            <Box m={3} sx={{ maxWidth: 200 }}>
+              <XKCD />
+            </Box>
+          ) : null}
+          <Typography color={fontColor} variant="body1">
+            Come back some other time. I am working it right now.
+          </Typography>
+        </Container>
+      </Box>
+    </Page>
+  );
+};
+
+NotFoundView.propTypes = {
+  fontColor: PropTypes.string,
+  backgroundImage: PropTypes.string,
+  xkcd: PropTypes.bool,
+};
 
 export default NotFoundView;
